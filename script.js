0d42d86ba30124c14426591769ccfa4ee80245f4
Mohammad MuaZam <mm9084@srmist.edu.in>
2020-10-02 20:13:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+(function(){
+    var list = document.querySelector('#list'),
+        enter = document.querySelector('#enter'),
+        item = document.querySelector('#item');
+    
+    enter.addEventListener('click',function(e){
+        e.preventDefault();
+        list.innerHTML += '<li>' + item.value + '</li>';
+        store();
+        item.value = "";
+    })
+    
+    list.addEventListener('click',function(e){
+        var t = e.target;
+        if(t.classList.contains('checked')){
+            t.parentNode.removeChild(t);
+        } else {
+            t.classList.add('checked');
+        }
+        store();
+    })
+    
+    function store() {
+        window.localStorage.myitems = list.innerHTML;
+    }
+    
+    function getValues() {
+        var storedValues = window.localStorage.myitems;
+        if(!storedValues) {
+            list.innerHTML = 
+                        '<li>Attend club meeting</li>'+
+                        '<li>Go to Market</li>'+
+                        '<li>Download movies</li>'+
+                        '<li>Attend webinar</li>';
+        }
+        else {
+            list.innerHTML = storedValues;
+        }
+    }
+    getValues();
+    
+    let greyC="#f1ebe5",redC="#ffb0b0",blueC="#a8d5ff",greenC="#bdff9f";
+    let body = document.getElementById("body"),title = document.getElementById("heading")
+    document.getElementById("default").addEventListener('click',function(){
+        body.style.backgroundColor = greyC;
+        title.style.color = greyC;
+    })
+    document.getElementById("red").addEventListener('click',function(){
+        body.style.backgroundColor = redC;
+        title.style.color = redC;
+    })
+    document.getElementById("blue").addEventListener('click',function(){
+        body.style.backgroundColor = blueC;
+        title.style.color = blueC;
+    })
+    document.getElementById("green").addEventListener('click',function(){
+        body.style.backgroundColor = greenC;
+        title.style.color = greenC;
+    })
+})();
